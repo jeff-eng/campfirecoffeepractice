9fe946506b72b6e34cecdc92d90fb58ddb3b3eba
Jeff Eng <jeff@jeffeng.me>
2016-02-12 10:51:54 -0800

    created location objects with properties: name, minCustomers, maxCustomers, avgCups, avgLbs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# campfirecoffeepractice
+Campfire Coffee Practice
+
+This project is a second attempt for practice with the concepts learned in Code Fellows Code 201. There is already an existing repository named campfire-coffee in my GitHub profile with this exact same project.  
+
+I will be changing some of aspects such as the data to make this project slightly different but as a whole the same in principle.
+
+2/12/2016
+- Creating object literals with methods
---
+++ js/app.js
@@ -0,0 +1,50 @@
+var hours = ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
+
+var pikePlace = {
+  name: 'Pike Place Market',
+  minCustomers: 14,
+  maxCustomers: 55,
+  avgCups: 1.2,
+  avgLbs: 3.7,
+
+};
+
+var capitolHill = {
+  name: 'Capitol Hill',
+  minCustomers: 32,
+  maxCustomers: 48,
+  avgCups: 3.2,
+  avgLbs: 0.4,
+};
+
+var seaPublicLibrary = {
+  name: 'Seattle Public Library',
+  minCustomers: 49,
+  maxCustomers: 75,
+  avgCups: 2.6,
+  avgLbs: 30.2,
+};
+
+var southLakeUnion = {
+  name: 'South Lake Union',
+  minCustomers: 38,
+  maxCustomers: 88,
+  avgCups: 1.3,
+  avgLbs: 3.7,
+};
+
+var seatacAirport = {
+  name: 'Sea-Tac Airport',
+  minCustomers: 68,
+  maxCustomers: 124,
+  avgCups: 1.1,
+  avgLbs: 2.7,
+};
+
+var websiteSales = {
+  name: 'Website Sales',
+  minCustomers: 3,
+  maxCustomers: 6,
+  avgCups: 0.0,
+  avgLbs: 6.7,
+};
