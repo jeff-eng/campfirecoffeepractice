4d0dd92c9aad5e3a2a79ba337acb87aad6e54c50
Jeff Eng <jeff@jeffeng.me>
2016-02-22 11:37:14 -0800

    created instances of location objects. prototype method for generate data. pseudocoded method for generating table.

--- js/app.js
+++ js/app.js
@@ -15,12 +15,35 @@ function Kiosk(minCustomers, maxCustomers, avgHourlyCups, avgHourlyLbs) {
 }
 
 Kiosk.prototype.generateData = function() {
-  //need to insert hourlyCustomers function, hourlyCups function, etc. that pushes data into each object's data arrays
   for(var i = 0; i < hours.length; i++) {
     this.hourlyCustomersArray[i].push(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers);
+    this.hourlyCupsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyCups);
+    this.hourlyTogoLbsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyLbs);
+    this.lbsFromCupsArray.push(this.hourlyCupsArray[i] / 20);
+    this.totalHourlyLbsArray.push(this.hourlyTogoLbsArray[i] + this.lbsFromCupsArray[i]);
+    this.totalDailyLbs += this.totalHourlyLbsArray[i];
   }
+  return this.totalDailyLbs;
 };
 
+Kiosk.prototype.renderData = function() {
+//write the instructions needed to render the table to the page
+//1)Create table element
+//2)Create table row element
+//3)Create table data element
+//4)Give the table data element content
+//5)Append table data element to the table row element
+//6)Append table row element to the table element
+//7)Append table element to the document
+};
+
+//Create instances
+var pikePlace = new Kiosk(14, 55, 1.2, 3.7);
+var capitolHill = new Kiosk(32, 48, 3.2, 0.4);
+var seaPublicLibrary = new Kiosk(49, 75, 2.6, 0.7);
+var southLakeUnion = new Kiosk(38, 88, 1.3, 3.7);
+var seatacAirport = new Kiosk(68, 124, 1.1, 2.7);
+var websiteSales = new Kiosk(3, 6, 0, 6.7);
 
 // var pikePlace = {
 //   name: 'Pike Place Market',
@@ -89,347 +112,7 @@ Kiosk.prototype.generateData = function() {
 //     ulEl.appendChild(displayTotalLbs);
 //   }
 // };
-//
-// var capitolHill = {
-//   name: 'Capitol Hill',
-//   minCustomers: 32,
-//   maxCustomers: 48,
-//   avgHourlyCups: 3.2,
-//   avgHourlyLbs: 0.4,
-//   totalDailyLbs: 0,
-//   hourlyCustomersArray: [],
-//   hourlyCupsArray: [],
-//   hourlyTogoLbsArray: [],
-//   lbsFromCupsArray: [],
-//   totalHourlyLbsArray: [],
-//
-//   hourlyCustomers: function () {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCustomersArray.push(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers);
-//     }
-//   },
-//   hourlyCups: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCupsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyCups);
-//     }
-//   },
-//   hourlyTogoLbs: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyTogoLbsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyLbs);
-//     }
-//   },
-//   lbsFromCups: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.lbsFromCupsArray.push(this.hourlyCupsArray[i] / 20);
-//     }
-//   },
-//   calcTotalHourlyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalHourlyLbsArray.push(this.hourlyTogoLbsArray[i] + this.lbsFromCupsArray[i]);
-//     }
-//   },
-//   calcTotalDailyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalDailyLbs += this.totalHourlyLbsArray[i];
-//     }
-//     return this.totalDailyLbs;
-//   },
-//   renderData: function() {
-//     this.hourlyCustomers();
-//     this.hourlyCups();
-//     this.hourlyTogoLbs();
-//     this.lbsFromCups();
-//     this.calcTotalHourlyLbs();
-//     this.calcTotalDailyLbs();
-//
-//     var ulEl = document.createElement('ul');
-//     var location = document.createTextNode(this.name);
-//     ulEl.appendChild(location);
-//     for(var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       var newText = document.createTextNode(hours[i] + ': ' + this.totalHourlyLbsArray[i].toFixed(1) + ' lbs [' + this.hourlyCustomersArray[i] + ' customers, ' + this.hourlyCupsArray[i].toFixed(1) + ' cups (' + this.lbsFromCupsArray[i].toFixed(1) + ' lbs), ' + this.hourlyTogoLbsArray[i].toFixed(1) + ' lbs to-go]');
-//       liEl.appendChild(newText);
-//       ulEl.appendChild(liEl);
-//     }
-//     document.body.appendChild(ulEl);
-//     var displayTotalLbs = document.createElement('li');
-//     displayTotalLbs.textContent = 'Total daily lbs at this location: ' + this.totalDailyLbs.toFixed(1) + ' lbs';
-//     ulEl.appendChild(displayTotalLbs);
-//   }
-// };
-//
-// var seaPublicLibrary = {
-//   name: 'Seattle Public Library',
-//   minCustomers: 49,
-//   maxCustomers: 75,
-//   avgHourlyCups: 2.6,
-//   avgHourlyLbs: 0.7,
-//   totalDailyLbs: 0,
-//   hourlyCustomersArray: [],
-//   hourlyCupsArray: [],
-//   hourlyTogoLbsArray: [],
-//   lbsFromCupsArray: [],
-//   totalHourlyLbsArray: [],
-//
-//   hourlyCustomers: function () {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCustomersArray.push(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers);
-//     }
-//   },
-//   hourlyCups: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCupsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyCups);
-//     }
-//   },
-//   hourlyTogoLbs: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyTogoLbsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyLbs);
-//     }
-//   },
-//   lbsFromCups: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.lbsFromCupsArray.push(this.hourlyCupsArray[i] / 20);
-//     }
-//   },
-//   calcTotalHourlyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalHourlyLbsArray.push(this.hourlyTogoLbsArray[i] + this.lbsFromCupsArray[i]);
-//     }
-//   },
-//   calcTotalDailyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalDailyLbs += this.totalHourlyLbsArray[i];
-//     }
-//     return this.totalDailyLbs;
-//   },
-//   renderData: function() {
-//     this.hourlyCustomers();
-//     this.hourlyCups();
-//     this.hourlyTogoLbs();
-//     this.lbsFromCups();
-//     this.calcTotalHourlyLbs();
-//     this.calcTotalDailyLbs();
-//
-//     var ulEl = document.createElement('ul');
-//     var location = document.createTextNode(this.name);
-//     ulEl.appendChild(location);
-//     for(var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       var newText = document.createTextNode(hours[i] + ': ' + this.totalHourlyLbsArray[i].toFixed(1) + ' lbs [' + this.hourlyCustomersArray[i] + ' customers, ' + this.hourlyCupsArray[i].toFixed(1) + ' cups (' + this.lbsFromCupsArray[i].toFixed(1) + ' lbs), ' + this.hourlyTogoLbsArray[i].toFixed(1) + ' lbs to-go]');
-//       liEl.appendChild(newText);
-//       ulEl.appendChild(liEl);
-//     }
-//     document.body.appendChild(ulEl);
-//     var displayTotalLbs = document.createElement('li');
-//     displayTotalLbs.textContent = 'Total daily lbs at this location: ' + this.totalDailyLbs.toFixed(1) + ' lbs';
-//     ulEl.appendChild(displayTotalLbs);
-//   }
-// };
-//
-// var southLakeUnion = {
-//   name: 'South Lake Union',
-//   minCustomers: 38,
-//   maxCustomers: 88,
-//   avgHourlyCups: 1.3,
-//   avgHourlyLbs: 3.7,
-//   totalDailyLbs: 0,
-//   hourlyCustomersArray: [],
-//   hourlyCupsArray: [],
-//   hourlyTogoLbsArray: [],
-//   lbsFromCupsArray: [],
-//   totalHourlyLbsArray: [],
-//
-//   hourlyCustomers: function () {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCustomersArray.push(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers);
-//     }
-//   },
-//   hourlyCups: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCupsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyCups);
-//     }
-//   },
-//   hourlyTogoLbs: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyTogoLbsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyLbs);
-//     }
-//   },
-//   lbsFromCups: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.lbsFromCupsArray.push(this.hourlyCupsArray[i] / 20);
-//     }
-//   },
-//   calcTotalHourlyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalHourlyLbsArray.push(this.hourlyTogoLbsArray[i] + this.lbsFromCupsArray[i]);
-//     }
-//   },
-//   calcTotalDailyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalDailyLbs += this.totalHourlyLbsArray[i];
-//     }
-//     return this.totalDailyLbs;
-//   },
-//   renderData: function() {
-//     this.hourlyCustomers();
-//     this.hourlyCups();
-//     this.hourlyTogoLbs();
-//     this.lbsFromCups();
-//     this.calcTotalHourlyLbs();
-//     this.calcTotalDailyLbs();
-//
-//     var ulEl = document.createElement('ul');
-//     var location = document.createTextNode(this.name);
-//     ulEl.appendChild(location);
-//     for(var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       var newText = document.createTextNode(hours[i] + ': ' + this.totalHourlyLbsArray[i].toFixed(1) + ' lbs [' + this.hourlyCustomersArray[i] + ' customers, ' + this.hourlyCupsArray[i].toFixed(1) + ' cups (' + this.lbsFromCupsArray[i].toFixed(1) + ' lbs), ' + this.hourlyTogoLbsArray[i].toFixed(1) + ' lbs to-go]');
-//       liEl.appendChild(newText);
-//       ulEl.appendChild(liEl);
-//     }
-//     document.body.appendChild(ulEl);
-//     var displayTotalLbs = document.createElement('li');
-//     displayTotalLbs.textContent = 'Total daily lbs at this location: ' + this.totalDailyLbs.toFixed(1) + ' lbs';
-//     ulEl.appendChild(displayTotalLbs);
-//   }
-// };
-//
-// var seatacAirport = {
-//   name: 'Sea-Tac Airport',
-//   minCustomers: 68,
-//   maxCustomers: 124,
-//   avgHourlyCups: 1.1,
-//   avgHourlyLbs: 2.7,
-//   totalDailyLbs: 0,
-//   hourlyCustomersArray: [],
-//   hourlyCupsArray: [],
-//   hourlyTogoLbsArray: [],
-//   lbsFromCupsArray: [],
-//   totalHourlyLbsArray: [],
-//
-//   hourlyCustomers: function () {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCustomersArray.push(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers);
-//     }
-//   },
-//   hourlyCups: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCupsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyCups);
-//     }
-//   },
-//   hourlyTogoLbs: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyTogoLbsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyLbs);
-//     }
-//   },
-//   lbsFromCups: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.lbsFromCupsArray.push(this.hourlyCupsArray[i] / 20);
-//     }
-//   },
-//   calcTotalHourlyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalHourlyLbsArray.push(this.hourlyTogoLbsArray[i] + this.lbsFromCupsArray[i]);
-//     }
-//   },
-//   calcTotalDailyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalDailyLbs += this.totalHourlyLbsArray[i];
-//     }
-//     return this.totalDailyLbs;
-//   },
-//   renderData: function() {
-//     this.hourlyCustomers();
-//     this.hourlyCups();
-//     this.hourlyTogoLbs();
-//     this.lbsFromCups();
-//     this.calcTotalHourlyLbs();
-//     this.calcTotalDailyLbs();
-//
-//     var ulEl = document.createElement('ul');
-//     var location = document.createTextNode(this.name);
-//     ulEl.appendChild(location);
-//     for(var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       var newText = document.createTextNode(hours[i] + ': ' + this.totalHourlyLbsArray[i].toFixed(1) + ' lbs [' + this.hourlyCustomersArray[i] + ' customers, ' + this.hourlyCupsArray[i].toFixed(1) + ' cups (' + this.lbsFromCupsArray[i].toFixed(1) + ' lbs), ' + this.hourlyTogoLbsArray[i].toFixed(1) + ' lbs to-go' + ']');
-//       ulEl.appendChild(liEl);
-//       liEl.appendChild(newText);
-//     }
-//     document.body.appendChild(ulEl);
-//     var displayTotalLbs = document.createElement('li');
-//     displayTotalLbs.textContent = 'Total daily lbs at this location: ' + this.totalDailyLbs.toFixed(1) + ' lbs';
-//     ulEl.appendChild(displayTotalLbs);
-//   }
-// };
-//
-// var websiteSales = {
-//   name: 'Website Sales',
-//   minCustomers: 3,
-//   maxCustomers: 6,
-//   avgHourlyCups: 0,
-//   avgHourlyLbs: 6.7,
-//   totalDailyLbs: 0,
-//   hourlyCustomersArray: [],
-//   hourlyCupsArray: [],
-//   hourlyTogoLbsArray: [],
-//   lbsFromCupsArray: [],
-//   totalHourlyLbsArray: [],
-//
-//   hourlyCustomers: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCustomersArray.push(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers);
-//     }
-//   },
-//   hourlyCups: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyCupsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyCups);
-//     }
-//   },
-//   hourlyTogoLbs: function() {
-//     for(var i = 0; i < hours.length; i++) {
-//       this.hourlyTogoLbsArray.push(this.hourlyCustomersArray[i] * this.avgHourlyLbs);
-//     }
-//   },
-//   lbsFromCups: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.lbsFromCupsArray.push(this.hourlyCupsArray[i] / 20);
-//     }
-//   },
-//   calcTotalHourlyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalHourlyLbsArray.push(this.hourlyTogoLbsArray[i] + this.lbsFromCupsArray[i]);
-//     }
-//   },
-//   calcTotalDailyLbs: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       this.totalDailyLbs += this.totalHourlyLbsArray[i];
-//     }
-//     return this.totalDailyLbs;
-//   },
-//   renderData: function() {
-//     this.hourlyCustomers();
-//     this.hourlyCups();
-//     this.hourlyTogoLbs();
-//     this.lbsFromCups();
-//     this.calcTotalHourlyLbs();
-//     this.calcTotalDailyLbs();
-//
-//     var ulEl = document.createElement('ul');
-//     var location = document.createTextNode(this.name);
-//     ulEl.appendChild(location);
-//     for (var i = 0; i < hours.length; i++) {
-//       var liEl = document.createElement('li');
-//       var newText = document.createTextNode(hours[i] + ': ' + this.totalHourlyLbsArray[i].toFixed(1) + ' lbs [' + this.hourlyCustomersArray[i] + ' customers, ' + this.hourlyCupsArray[i] + ' cups (' + this.lbsFromCupsArray[i].toFixed(1) + ' lbs), ' + this.hourlyTogoLbsArray[i].toFixed(1) + ' lbs to-go]');
-//       ulEl.appendChild(liEl);
-//       liEl.appendChild(newText);
-//     }
-//     document.body.appendChild(ulEl);
-//     var displayTotalLbs = document.createElement('li');
-//     displayTotalLbs.textContent = 'Total daily lbs at this location: ' + this.totalDailyLbs.toFixed(1) + ' lbs';
-//     ulEl.appendChild(displayTotalLbs);
-//   }
-// };
-//
+
 // pikePlace.renderData();
 // capitolHill.renderData();
 // seaPublicLibrary.renderData();
